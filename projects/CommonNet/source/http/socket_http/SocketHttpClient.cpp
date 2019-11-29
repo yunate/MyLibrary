faf899c874fe09025e3b3c05e6e5875170741c56
yunate <2997683428@qq.com>
2019-11-29 11:12:25 +0800

    1

--- projects/CommonNet/source/http/socket_http/SocketHttpClient.cpp
+++ projects/CommonNet/source/http/socket_http/SocketHttpClient.cpp
@@ -1,5 +1,8 @@
 
 #include "SocketHttpClient.h"
+#include "socket_common\SocketUtils.h"
+
+#include "Url\DogUrl.h"
 
 SocketHttpClient::SocketHttpClient()
 {
@@ -7,4 +10,81 @@ SocketHttpClient::SocketHttpClient()
 
 SocketHttpClient::~SocketHttpClient()
 {
+}
+
+bool SocketHttpClient::Get(const DogStringA& urlStr)
+{
+    DogUrl url;
+    ParseUrl(urlStr, url);
+
+    if (!url.IsValid())
+    {
+        return false;
+    }
+
+    // 获得ip
+    std::vector<DogStringA> ips;
+    SocketUtils::GetIpByHostName(url.m_host, ips);
+    if (ips.size() == 0)
+    {
+        return false;
+    }
+
+    SocketTcpClient client(ips[0], url.m_port);
+
+    if (!client.Connect())
+    {
+        return false;;
+    }
+
+    // 拼接
+    DogStringA strHttpHead;
+    strHttpHead.append("GET / HTTP/1.1\r\n");
+    strHttpHead.append(url.m_scheme);
+    strHttpHead.append("/1.1\r\n");
+    strHttpHead.append("Accept: */*\r\n");
+    strHttpHead.append("Accept-Language: cn\r\n");
+    strHttpHead.append("User-Agent: Mozilla/4.0\r\n");
+    strHttpHead.append("Host: ");
+    strHttpHead.append(url.m_host);
+    strHttpHead.append("\r\n");
+    strHttpHead.append("Cache-Control: no-cache\r\n");
+    strHttpHead.append("Connection: Keep-Alive\r\n");
+    strHttpHead.append("\r\n\r\n");
+
+    client.SendMsg(strHttpHead);
+    std::string msg = "";
+    client.RcvMsg([&msg](char* buff, unsigned int buffSize)
+    {
+        // 如果想手动结束的话，返回false 
+        msg.append(buff, buffSize);
+        return true;
+    });
+
+    return false;
+}
+
+bool SocketHttpClient::Post(const DogStringA& url)
+{
+    return false;
+}
+
+void SocketHttpClient::SetUploadPercentCallBack(const DogPercentCallBack& callback)
+{
+    m_upLoadPercentCallBack = callback;
+}
+
+void SocketHttpClient::SetDownloadPercentCallBack(const DogPercentCallBack& callback)
+{
+    m_downLoadPercentCallBack = callback;
+}
+
+void SocketHttpClient::SetUploadStream(const SPDogStream& stream)
+{
+    m_upLoadStream = stream;
+}
+
+void SocketHttpClient::SetDownloadStream(const SPDogStream& stream)
+{
+    m_downLoadLoadStream = stream;
 }
--- projects/CommonNet/source/http/socket_http/SocketHttpClient.h
+++ projects/CommonNet/source/http/socket_http/SocketHttpClient.h
@@ -3,6 +3,23 @@
 #define _SOCKET_HTTP_CLIENT_H__
 
 #include "socket_tcp/SocketTcpClient.h"
+#include "stream/IDogStream.h"
+#include "typedef/DogInterger.h"
+
+#include <functional>
+#include <memory>
+
+/** 进度回调函数
+    @functional:
+        @param [in] handledSize 已经处理的总大小
+        @param [in] allSize 总大小
+        @return 返回false时候，结束过程进度
+*/
+using  DogPercentCallBack = std::function<bool(const u64 handledSize, const u64 allSize)>;
+
+/** 流的智能指针
+*/
+using SPDogStream = std::shared_ptr<IDogStream>;
 
 class SocketHttpClient :
     public SocketTcpClient
@@ -15,8 +32,55 @@ public:
     /** 析构函数
     */
     ~SocketHttpClient();
-public:
 
+public:
+    /** get 方法
+    @param [in] urlStr 
+    */
+    bool Get(const DogStringA& urlStr);
+
+    /** post 方法
+    @param [in] urlStr
+    */
+    bool Post(const DogStringA& urlStr);
+
+public:
+    /** 设置下载进度回调
+    @param [in] callback: 上传进度回调
+    */
+    void SetUploadPercentCallBack(const DogPercentCallBack& callback);
+
+    /** 设置下载进度回调
+    @param [in] callback: 下载进度回调
+    */
+    void SetDownloadPercentCallBack(const DogPercentCallBack& callback);
+
+    /** 设置上传流
+    @param [in] stream: 上传流
+    */
+    void SetUploadStream(const SPDogStream& stream);
+
+    /** 设置下载流
+    @param [in] stream: 下载流
+    */
+    void SetDownloadStream(const SPDogStream& stream);
+
+protected:
+    /** 上传进度回调
+    */
+    DogPercentCallBack m_upLoadPercentCallBack;
+
+    /** 下载进度回调
+    */
+    DogPercentCallBack m_downLoadPercentCallBack;
+
+    /** 上传流
+    */
+    SPDogStream m_upLoadStream;
+
+    /** 下载流
+    */
+    SPDogStream m_downLoadLoadStream;
 };
 
 
